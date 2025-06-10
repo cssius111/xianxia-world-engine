747ad013d8bc2dbba255a907436b13653ed84cdc
Pinle Chen <90313887+cssius111@users.noreply.github.com>
2025-06-10 09:55:48 +0800

    Add SmartCache get_or_compute and decorator (#54)

--- xwe/core/optimizations/__init__.py
+++ xwe/core/optimizations/__init__.py
@@ -2,7 +2,13 @@
 
 from .expression_jit import ExpressionJITCompiler, ExpressionBenchmark
 from .smart_cache import SmartCache, CacheableFunction
-from .async_event_system import AsyncEventSystem, AsyncEvent, EventBus
+
+try:  # 兼容缺少异步事件系统实现的情况
+    from .async_event_system import AsyncEventSystem, AsyncEvent, EventBus
+except Exception:  # pragma: no cover - allow missing module
+    AsyncEventSystem = None
+    AsyncEvent = None
+    EventBus = None
 
 __all__ = [
     'ExpressionJITCompiler',
