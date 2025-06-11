92a2da139ead82e69f2d070d2e966095f30d1874
Pinle Chen <90313887+cssius111@users.noreply.github.com>
2025-06-11 10:02:04 +0800

    expand attribute fields

---
+++ tests/test_character_attributes.py
@@ -0,0 +1,23 @@
+import pytest
+from xwe.core.attributes import CharacterAttributes
+
+
+def test_extra_attribute_access():
+    attrs = CharacterAttributes()
+    # attack_power computed in __post_init__ should be accessible
+    assert isinstance(attrs.attack_power, (int, float))
+    # dynamically override attribute
+    attrs.attack_power = 123
+    assert attrs.attack_power == 123
+
+
+def test_missing_attribute_error():
+    attrs = CharacterAttributes()
+    with pytest.raises(AttributeError):
+        _ = attrs.nonexistent_attr
+
+
+def test_max_cultivation_default():
+    """Ensure newly created attributes include max_cultivation."""
+    attrs = CharacterAttributes()
+    assert attrs.max_cultivation == 100
