773dd418d8951bf85d7df9c0bc69de42c029de5d
Pinle Chen <90313887+cssius111@users.noreply.github.com>
2025-06-18 11:26:39 +0800

    Delete archive/backups directory

--- archive/backups/backup_20250612_220028/__init__.py
+++
@@ -1,9 +0,0 @@
-# xwe/__init__.py
-"""
-仙侠世界引擎 (XianXia World Engine)
-
-一个基于文本的仙侠世界模拟器。
-"""
-
-__version__ = '2.0.0'
-__author__ = 'XWE Team'
--- archive/backups/backup_20250612_220028/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Internal test helpers for XWE."""
-
-from xwe.utils.dotenv_helper import load_dotenv
-
-load_dotenv()
-# tests
-
-# XWE内部测试（如果有的话）
